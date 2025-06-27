9644993bf15ca597e404965595a9a7f674128943
Kim Rasmussen <36648257+kim-ras@users.noreply.github.com>
2025-06-27 17:58:51 +0200

    More doc

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ESP32BattSensor
-This is to build a simple weater station to be used for home automation. The projevt is build for Home Assistans but the ZigBee code can be used genericly.
+This is to build a simple weather station to be used for home automation. The project is build for Home Assistant but the ZigBee code can be used generic.
 This is how I have set up my Weather station data
 
 ![alt text](<images/Home Assistant - weather station.png>)
@@ -10,4 +10,33 @@ There is 2 software versions:
 
 
 ## Hardware
-This is based on a ESP32C6 from Seeed Studio and a BME280 sensor fro Temperture, Humidity and Barameoric pressure.
+This is based on a ESP32C6 from Seeed Studio and a BME280 sensor fro Temperature, Humidity and Barometric pressure.
+
+## ZigBee
+### Arduino project setup
+Clone repository, and open the ird file.
+In the Arduino UI set the following 
+* Before Compile/Verify, select the correct board: `Tools -> Board` - **XIAO ESP32c6**
+* Select the End device Zigbee mode: `Tools -> Zigbee mode: Zigbee ED (end device)`
+* Select Partition Scheme for Zigbee: `Tools -> Partition Scheme: Zigbee 4MB with spiffs`
+* Select the COM port: `Tools -> Port: xxx` where the `xxx` is the detected COM port.
+* Optional: Set debug level to verbose to see all logs from Zigbee stack: `Tools -> Core Debug Level: Verbose`.
+
+If you have changed any of the pins (Battery and the SLC bus). Also if you are using another ESP32c6 then check accordantly.
+
+Open compile and upload the code to the ESP32c6.. 
+
+### Connecting the first time
+The sensor will be in paring mode when  
+
+### Reset to factory
+If the device is already registered in a home automation -- delete this entry..
+
+
+![alt text](images/ESP32c6.png)
+To reset connection do the following on the ESP32:
+<ol>
+  <li>Reset (button) & after Boot button -- IMPORTANT! Du not use a metal object to push the buttons, it can damage the device if you touches any other terminal on the board. Use a plastic or a nail if your finger is to big to get to the tiny button </li>
+  <li>Wait until the yellow light (User led) Is solid</li>
+  <li>Wait 5 seconds then reset the device and start the search as above</li>
+</ol>
---
+++ Zigbee_Temp_Hum_press_Sensor_Sleepy/Zigbee_Temp_Hum_press_Sensor_Sleepy.ino
@@ -0,0 +1,210 @@
+// Copyright 2024 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Based on a number of exampeles that comes with the Espressif tool kit for Aurdrine.
+// 
+// For deep down documentation see Github: https://github.com/TheTechDane/ESP32BattSensor
+//
+// It is based on a Seeed Studio ESP32c6 - and BME280 sensoe and a 18650 battery.
+//
+//  Make sure you check the Battery pin, and the SLC pins before deployiing the code.
+// 
+
+#ifndef ZIGBEE_MODE_ED
+#error "Zigbee end device mode is not selected in Tools->Zigbee mode"
+#endif
+
+#include "Zigbee.h"
+#define BATT_EMPTY 2.90
+#define BATT_FULL 4.02
+#define BATT_PIN A2
+#define SDA_PIN D4
+#define SCL_PIN D5
+#define BME280_ADDR 0x76
+#define button = BOOT_PIN;            //The factory reste pin.
+#define USER_LED = LED_BUILTIN;
+
+Adafruit_BME280 bme; // I2C
+
+/* Zigbee temperature + humidity sensor configuration */
+#define TEMP_SENSOR_ENDPOINT_NUMBER 10
+#define PRESSURE_SENSOR_ENDPOINT_NUMBER 11
+
+#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  55         /* Sleep for 55s will + 5s delay for establishing connection => data reported every 1 minute */
+
+ZigbeeTempSensor zbTempSensor = ZigbeeTempSensor(TEMP_SENSOR_ENDPOINT_NUMBER);
+ZigbeePressureSensor zbPressureSensor = ZigbeePressureSensor(PRESSURE_SENSOR_ENDPOINT_NUMBER);
+
+/************************ Temp sensor *****************************/
+void meausureAndSleep() {
+  // Measure temperature sensor value
+  float temperature = temperatureRead();
+  // Use temperature value as humidity value to demonstrate both temperature and humidity
+  float humidity = temperature;
+  float pressure = 1000 + temperature;
+  int batt = getCurrentBatteryPercent();
+
+  // Update temperature and humidity values in Temperature sensor EP
+  zbTempSensor.setTemperature(temperature);
+  zbTempSensor.setHumidity(humidity);
+  zbTempSensor.report();
+  Serial.printf("Reported temperature: %.2f°C, Humidity: %.2f%%\r\n", temperature, humidity);
+
+  zbTempSensor.setBatteryPercentage(batt);
+  zbTempSensor.reportBatteryPercentage();
+  Serial.printf("Reported battery: %i%%\r\n", batt);
+
+  Serial.printf("Updating pressure sensor value to %f.0 hPa\r\n", pressure);
+  zbPressureSensor.setPressure(pressure);
+
+  // Add small delay to allow the data to be sent before going to sleep
+  delay(100);
+
+  // Put device to deep sleep
+ // Serial.println("Going to sleep now");
+ // esp_deep_sleep_start();
+ delay(10000);
+}
+
+/********************* Arduino functions **************************/
+void setup() {
+  Serial.begin(115200);
+
+  // Init button switch
+  pinMode(button, INPUT_PULLUP);
+  pinMode(USER_LED, OUTPUT);
+  pinMode(BATT_PIN, INPUT);         // ADC
+
+  //IC2 - Initiate
+  delay(1000);
+  Wire.begin();
+  Wire.beginTransmission(BME280_ADDR);
+  int error = Wire.endTransmission();
+  if (error == 0) {
+      Serial.printf("I2C device found at address 0x%02X\n", BME280_ADDR);
+  }
+
+  bool status = bme.begin(BME280_ADDR);
+  if (!status) {
+    Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
+    //while (1);
+  } else {
+    Serial.println(F("BME280 Sensor Initialized:"));
+  }
+
+  // Configure the wake up source and set to wake up every 5 seconds
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+  // Optional: set Zigbee device name and model
+  zbTempSensor.setManufacturerAndModel("TheTechDane", "TempHumidSensor");
+
+  // Set minimum and maximum temperature measurement value (10-50°C is default range for chip temperature measurement)
+  zbTempSensor.setMinMaxValue(10, 50);
+
+  // Set tolerance for temperature measurement in °C (lowest possible value is 0.01°C)
+  zbTempSensor.setTolerance(1);
+
+  // The value can be also updated by calling zbTempSensor.setBatteryPercentage(percentage) anytime
+  zbTempSensor.setPowerSource(ZB_POWER_SOURCE_BATTERY, 100);
+
+  // Add humidity cluster to the temperature sensor device with min, max and tolerance values
+  zbTempSensor.addHumiditySensor(0, 100, 1);
+
+
+  // Optional: set Zigbee device name and model
+  zbPressureSensor.setManufacturerAndModel("TheTechDane", "PressureSensor");
+
+  // Set minimum and maximum pressure measurement value in hPa
+  zbPressureSensor.setMinMaxValue(0, 10000);
+
+  // Optional: Set tolerance for pressure measurement in hPa
+  zbPressureSensor.setTolerance(1);
+
+  // Add endpoint to Zigbee Core
+  Zigbee.addEndpoint(&zbTempSensor);
+  Zigbee.addEndpoint(&zbPressureSensor);
+
+  // Create a custom Zigbee configuration for End Device with keep alive 10s to avoid interference with reporting data
+  esp_zb_cfg_t zigbeeConfig = ZIGBEE_DEFAULT_ED_CONFIG();
+  zigbeeConfig.nwk_cfg.zed_cfg.keep_alive = 10000;
+
+  // For battery powered devices, it can be better to set timeout for Zigbee Begin to lower value to save battery
+  // If the timeout has been reached, the network channel mask will be reset and the device will try to connect again after reset (scanning all channels)
+  Zigbee.setTimeout(10000);  // Set timeout for Zigbee Begin to 10s (default is 30s)
+
+  // When all EPs are registered, start Zigbee in End Device mode
+  if (!Zigbee.begin(&zigbeeConfig, false)) {
+    Serial.println("Zigbee failed to start!");
+    Serial.println("Rebooting...");
+    ESP.restart();  // If Zigbee failed to start, reboot the device and try again
+  }
+  Serial.println("Connecting to network");
+  while (!Zigbee.connected()) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+  Serial.println("Successfully connected to Zigbee network");
+
+  // Delay approx 1s (may be adjusted) to allow establishing proper connection with coordinator, needed for sleepy devices
+  delay(1000);
+}
+
+void loop() {
+  // Checking button for factory reset
+  Serial.println("Button : " + (digitalRead(button) == LOW));
+  if (digitalRead(button) == LOW) {  // Push button pressed
+    // Key debounce handling
+    delay(100);
+    int startTime = millis();
+    while (digitalRead(button) == LOW) {
+      delay(50);
+      if ((millis() - startTime) > 10000) {
+        // If key pressed for more than 10secs, factory reset Zigbee and reboot
+        Serial.println("Resetting Zigbee to factory and rebooting in 1s.");
+        delay(1000);
+        // Optional set reset in factoryReset to false, to not restart device after erasing nvram, but set it to endless sleep manually instead
+        Zigbee.factoryReset(false);
+        Serial.println("Going to endless sleep, press RESET button or power off/on the device to wake up");
+        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_TIMER);
+        esp_deep_sleep_start();
+      }
+    }
+  }
+
+  // Call the function to measure temperature and put the device to sleep
+  meausureAndSleep();
+}
+
+int getCurrentBatteryPercent() {
+  int battPercent = 1;
+  uint32_t Vbatt = 0;
+  for(int i = 0; i < 16; i++) {
+    Vbatt = Vbatt + analogReadMilliVolts(BATT_PIN); // ADC with correction   
+  }
+  float Vbattf = 2* Vbatt / 16 / 1000.0;     // attenuation ratio 1/2, mV --> V
+  Serial.print("Battery Voltage: ");
+  Serial.print(Vbattf, 3);
+
+  // Calculate the voltage range
+  float voltageRange = BATT_FULL - BATT_EMPTY;
+  // Calculate the battery percentage
+  battPercent = ((Vbattf - BATT_EMPTY) / voltageRange) * 100.0;
+
+  Serial.print(" Battery Percentage: ");
+  Serial.println(battPercent);
+
+  return battPercent;
+}
