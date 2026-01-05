da204e1874b7ee74717e2c1e145ca4e6d97ffcd1
Kim Rasmussen <36648257+kim-ras@users.noreply.github.com>
2026-01-05 09:21:54 +0100

    Ajustments to the INO - First test outside

--- ESPNow/ESPNow-Sensor/ESPNow-Sensor.ino
+++ ESPNow/ESPNow-Sensor/ESPNow-Sensor.ino
@@ -16,6 +16,8 @@
     Helpfull links: https://randomnerdtutorials.com/esp-now-esp32-arduino-ide/
 
 */
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 #include "ESP32_NOW.h"
 #include "WiFi.h"
 #define LOGLEVEL LOGLEVEL_DEBUGING
@@ -24,8 +26,22 @@
 //IMPORTANT to set the right MAC address of the reciving gateway..
 uint8_t broadcastAddress[] = {0x34, 0x85, 0x18, 0x00, 0x2D, 0xE4};
 // Broadcast -- uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-char senderID[11] = "ESPNow-tst";
+char senderID[11] = "ESPNow-001";
 #define ESPNowChannal 1
+
+#define BATT_EMPTY 2.90
+#define BATT_FULL 4.02
+#define BATT_PIN A0
+#define SDA_PIN D4
+#define SCL_PIN D5
+#define BME280_ADDR 0x76
+#define button BOOT_PIN            //The factory reste pin.
+#define USER_LED LED_BUILTIN
+#define nrBattSamples 5            //Battery meassurement - Was 16 to start with
+#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
+//#define TIME_TO_SLEEP  115         /* Sleep for 115s will + 5s delay for establishing connection => data reported every 2 minutes */
+#define TIME_TO_SLEEP  20         /* Sleep for 20s for testing */
+#define RESET_DELAY 10            /* If reset was pressed wait 10 sec, if it was for programming */
 
 typedef struct sensorData {
   char senderID[11];
@@ -36,9 +52,13 @@ typedef struct sensorData {
   int batt;         // Battery Presentage
 } sensorData;
 
-sensorData mySensorData;              // The Sensor data sending
 esp_now_peer_info_t peerInfo;         // For the payload that returns, and the peer registration.
 long t = 0;
+Adafruit_BME280 bme; // I2C
+// A variable to hold the reset cause flags
+bool wasResetRestart = false;
+int rebootTime = 0;
+bool readyToSleep = false;
 
 void readMacAddress(){
   uint8_t baseMac[6];
@@ -55,10 +75,94 @@ void readMacAddress(){
   loglnD(cFormattedMac);
 }
 
+/******************************************************************
+*
+*  Do the Actual Measurement 
+*
+******************************************************************/
+void doMeasurement() {
+  //IC2 - Initiate
+  Wire.begin();
+  Wire.beginTransmission(BME280_ADDR);
+  int error = Wire.endTransmission();
+  if (error == 0) {
+      Serial.printf("I2C device found at address 0x%02X\n", BME280_ADDR);
+  }
+
+  bool status = bme.begin(BME280_ADDR);
+  if (!status) {
+    loglnD("Could not find a valid BME280 sensor, check wiring!");
+    //while (1);
+  } else {
+    loglnD("BME280 Sensor Initialized:");
+  }
+
+  sensorData mySensorData;                      // The Sensor data sending
+  strncpy(mySensorData.senderID, senderID, sizeof(senderID));
+  mySensorData.temp = bme.readTemperature();    // Measure temperature sensor value
+  mySensorData.humi = bme.readHumidity();          // Messure humidity value
+  mySensorData.pres = round(bme.readPressure() / 100.0F); // Convert Pa to hPa
+  mySensorData.batt = getCurrentBatteryPercent();
+
+  char sFormatted[100];
+  sprintf(sFormatted,"Meausured Temp:%1f Humitity:%1f Preassure:%d Batt:%d", mySensorData.temp, mySensorData.humi, mySensorData.pres, mySensorData.batt);
+  loglnD(sFormatted);
+
+  //Send
+  esp_now_send(broadcastAddress, (uint8_t *) &mySensorData, sizeof(mySensorData));
+}
+
+void gotoSleep() {
+  //Put Sensor to sleep
+  BME280_Sleep(BME280_ADDR);
+  // Put device to deep sleep
+  loglnI("Going to sleep now");
+  esp_deep_sleep_start();
+}
+
+void blinkLEDOnce(int iBlinkDelay = 300) {
+    digitalWrite(USER_LED, LOW);  // turn the LED on 
+    delay(iBlinkDelay);                      
+    loglnD("Blink..");  
+    digitalWrite(USER_LED, HIGH);   // turn the LED off 
+    delay(iBlinkDelay);   
+}
+
+void blinkLED(int iBlinkDelay = 300) {
+    for (int i=0; i<3; i++) {
+      blinkLEDOnce(iBlinkDelay);
+  }
+}
+
+/********************* SETUP **************************/
 void setup() {
   Serial.begin(115200);
   Serial.println("Current log level is :" + String(LOGLEVEL));
   loglnV("Starting");
+ 
+   //
+  // Check Reboot reason
+  //
+  int iBootReason = esp_reset_reason();               // but I still can use and find the ummerical value
+  logD("Reset/Boot Reason was: "); loglnD( iBootReason );
+  if ( iBootReason == ESP_RST_POWERON ) {             // Reset due to power-on event.
+    logD("Reboot was because of Power-On!!  - Waiting ");
+    logD(RESET_DELAY);
+    loglnD(" Sec.");
+    wasResetRestart = true;
+  }
+  if ( iBootReason == ESP_RST_DEEPSLEEP) {                  // Reset due to power-on event.
+    loglnD("Reboot was because of DeepSleep!!");
+  }
+  if ( iBootReason == ESP_RST_EXT ) {                  // Reset due to power-on event.
+    loglnD("Reboot was because of External reset!!");
+  }
+  rebootTime = millis();
+  
+  // Configure the wake up source and set to wake up every xx secunds
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+ 
+ 
   pinMode(LED_BUILTIN, OUTPUT);
   WiFi.mode(WIFI_STA);   //Use special ESPNow Mode
   
@@ -82,21 +186,25 @@ void setup() {
   }
   esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
   //esp_now_register_recv_cb(OnDataRecv);
+  
 }
 
+/******************************* LOOP ************************/
 void loop() {
   // put your main code here, to run repeatedly:
-  if(millis() - t > 2000){
-    t = millis();
-
-    strncpy(mySensorData.senderID, senderID, sizeof(senderID));
-    mySensorData.type = 1;    // Type of payload
-    mySensorData.temp = random(2000,5000)/100.0;//20.00 ~ 50.00
-    mySensorData.humi = random(2000,5000)/100.0;
-    mySensorData.pres = 1000;
-    mySensorData.batt = 67;
-
-    esp_now_send(broadcastAddress, (uint8_t *) &mySensorData, sizeof(mySensorData));
+  if (wasResetRestart) {
+    //Check if reset was pressed - and time has been passed
+    if ((millis() - rebootTime) < (RESET_DELAY * 1000)) {
+      blinkLED(1000);
+    } else {
+      wasResetRestart = false;
+    }
+  } else {
+    // Call the function to measure temperature and put the device to sleep
+    blinkLEDOnce();
+    doMeasurement();
+    delay(100);    //To ensure Send
+    gotoSleep();
   }
 }
 
@@ -105,18 +213,16 @@ void loop() {
   Status if data was send correctly
 *******************************************************************************/
 void OnDataSent(uint8_t *mac, esp_now_send_status_t status) {
-
   char cFormattedMac[21];
   sprintf(cFormattedMac,"%02x:%02x:%02x:%02x:%02x:%02x\n",mac[0], mac[1], mac[2],mac[3],mac[4],mac[5]);
   logD("Sent to MAC Address:");
   loglnD(cFormattedMac);
+  logI("Send status:");
+  loglnI(status);
 
+  readyToSleep = true;
 
-
-logI("Send status:");
-loglnI(status);
-
-   if(status == ESP_NOW_SEND_SUCCESS) {
+   if(status == ESP_NOW_SEND_SUCCESS) {   //Not sure why I get a 1 and it does send !! ?? !!
      loglnI("Send Successfull!");
    }
    else {
@@ -131,4 +237,47 @@ loglnI(status);
 *******************************************************************************/
 void OnDataRecv(uint8_t * mac, uint8_t * data, uint8_t len) {
 
+}
+
+
+float getBatteryVoltage() {
+  uint32_t Vbatt = 0;
+  for(int i = 0; i < nrBattSamples; i++) {
+    Vbatt = Vbatt + analogReadMilliVolts(BATT_PIN); // ADC with correction   
+  }
+  float Vbattf = 2* Vbatt / nrBattSamples / 1000.0;     // attenuation ratio 1/2, mV --> V
+  return Vbattf;
+}
+
+int getCurrentBatteryPercent() {
+  float Vbattf = getBatteryVoltage();
+  logD("Battery Voltage: ");
+  logD(Vbattf);
+
+  // Calculate the voltage range
+  float voltageRange = BATT_FULL - BATT_EMPTY;
+  // Calculate the battery percentage
+  int battPercent = 0;
+  if ((Vbattf - BATT_EMPTY) > 0.0) battPercent = ((Vbattf - BATT_EMPTY) / voltageRange) * 100.0;
+
+  logD(" Battery Percentage: ");
+  loglnD(battPercent);
+
+  return battPercent;
+}
+
+void BME280_Sleep(int device_address) {
+  // FROM: https://github.com/G6EJD/BME280-Sleep-and-Address-change/blob/master/BME280_Sleep.ino
+  //
+  // BME280 Register 0xF4 (control measurement register) sets the device mode, specifically bits 1,0
+  // The bit positions are called 'mode[1:0]'. See datasheet Table 25 and Paragraph 3.3 for more detail.
+  // Mode[1:0]  Mode
+  //    00      'Sleep'  mode
+  //  01 / 10   'Forced' mode, use either '01' or '10'
+  //    11      'Normal' mode
+  loglnI("BME280 to Sleep mode...");
+  Wire.beginTransmission(device_address);
+  Wire.write((uint8_t)0xF4);       // Select Control Measurement Register
+  Wire.write((uint8_t)0b00000000); // Send '00' for Sleep mode
+  Wire.endTransmission();
 }
